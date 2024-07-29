f2dbb138939d363c41be8fc29fac55ad65f4b4fd
Oleg <larog02@mail.ru>
2024-07-29 17:55:01 +0300

    home4 fix value eror

--- src/phone.py
+++ src/phone.py
@@ -9,6 +9,13 @@ class Phone(Item):
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
-    def check_number_of_sim(self, number_of_sim):
-        if number_of_sim <= 0:
+    @property
+    def number_of_sim(self):
+        return self._number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if value <= 0:
             raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        self._number_of_sim = value
+
